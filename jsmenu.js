bf5189afe112e65341db87f0d2dd0aaeac2b1c94
Diane <73044259+diameterr@users.noreply.github.com>
2024-12-06 21:56:42 -0600

    Add files via upload

---
+++ jsmenu.js
@@ -0,0 +1,22 @@
+function changeScr(){
+    document.getElementById("imgid").classList.toggle("bounce");
+    document.getElementById("imgid").src="garfwalker.png";
+}
+function changeScr2(){
+    document.getElementById("imgid").classList.toggle("bounce");
+    document.getElementById("imgid").src="toplogo.png";
+}
+function changeScr3(){
+    document.getElementById("imgid").classList.toggle("bounce");
+    document.getElementById("imgid").src="gintama2.png";
+}
+function changeScr4(){
+    document.getElementById("imgid").classList.toggle("bounce");
+    document.getElementById("imgid").src="garfstat.png";
+}
+
+function changeScr5(){
+    document.getElementById("imgid").classList.toggle("bounce");
+    document.getElementById("imgid").src="tankbotimg.png";
+}
+
