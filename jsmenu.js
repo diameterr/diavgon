079fcb3b4e793f03618127cafe6c22f2e5e6e6a7
Diane <73044259+diameterr@users.noreply.github.com>
2025-01-07 01:49:43 -0600

    Add files via upload
    Optimized buttons for onclick anywhere on buttons, adjusted formatting and added dropdown menu

--- jsmenu.js
+++ jsmenu.js
@@ -1,3 +1,17 @@
+
+    // JavaScript to handle the collapse feature
+const toggleButton = document.getElementById('toggleCollapse');
+const buttonContainer = document.getElementById('buttonContainer');
+
+toggleButton.addEventListener('click', () => {
+      // Toggle visibility classes
+    buttonContainer.classList.toggle('hidden');
+    buttonContainer.classList.toggle('visible');
+
+      // Rotate the dropdown icon
+    toggleButton.classList.toggle('rotated');
+    });
+
 function changeScr(){
     document.getElementById("imgid").classList.toggle("bounce");
     document.getElementById("imgid").src="garfwalker.png";
